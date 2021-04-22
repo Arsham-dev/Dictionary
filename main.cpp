a56373256dcc32876e9b7ef6a1229fb118f74d3c
ArShAm <arsham.ramezani@gmail.com>
2021-04-22 18:22:40 +0430

    Add addWord

--- main.cpp
+++ main.cpp
@@ -13,18 +13,19 @@ words *creatWord(string word, words *synonym) {
     return node;
 }
 
+void addWord(words *&head, words *node);
+
 void printWord(words *node);
 
 void printAllWords(words *head);
 
 int main() {
-    auto item=creatWord("ali", nullptr);
-    printAllWords(item);
+
     return 0;
 }
 
 void printWord(words *node) {
-    cout << "Word: " << node->word << ": ";
+    cout << "Word: " << node->word << "\tsynonym: ";
     words *temp = node->synonym;
     while (temp != nullptr) {
         cout << temp->word << ", ";
@@ -39,4 +40,22 @@ void printAllWords(words *head) {
         printWord(temp);
         temp = temp->next;
     }
+}
+
+void addWord(words *&head, words *node) {
+    auto temp = head;
+    if (head == nullptr) {
+        head = node;
+        return;
+    }
+
+    while (temp->next != nullptr && temp->next->word.compare(node->word) < 0)
+        temp = temp->next;
+    if (temp->next == nullptr)
+        temp->next = node;
+    else {
+        node->next = temp->next;
+        temp->next = node;
+    }
+
 }
